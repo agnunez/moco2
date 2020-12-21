3198be8d7decc656382438db6fd341594d7de7dd
agnunez <anunez@gmail.com>
2020-12-21 18:47:59 +0000

    Update CM1106calibration.py
    Print df3 byte containing range mode and heating bits

--- calibration/CM1106calibration.py
+++ calibration/CM1106calibration.py
@@ -7,11 +7,14 @@ def cs(s):
     return 256 - cs % 256
 
 def readco2():
+    debug = True
     ser = serial.Serial('COM3', 9600, timeout=4)
     readcmd = [0x11,0x01,0x01,0xED]
     ser.write(serial.to_bytes(readcmd))
     res = ser.read(8)        # read up to 8 bytes (timeout)
     ppm = res[3]*256+res[4]
+    if debug:
+        print("df3: ", res[5]," ppm: ", end='')
     if (cs(res) != res[7]):
         ser.flushInput()
         print(res)
